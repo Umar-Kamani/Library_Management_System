23d9a6f8a0ccdb4ee9ffaa33b7b573763995123e
Umar Kamani <umar.kamani@gmail.com>
2026-09-09 23:13:40 +0400

    Added User class and its subclasses student and faculty

---
+++ src/main/java/Models/faculty.java
@@ -0,0 +1,9 @@
+package Models;
+
+public class faculty extends User {
+
+    public faculty(int id, String name, String phone, String email){
+        super(id, name, phone, email);
+    }
+
+}
---
+++ src/main/java/Models/student.java
@@ -0,0 +1,10 @@
+package Models;
+
+public class student extends User {
+
+    public student(int id, String name, String phone, String email){
+        super(id, name, phone, email);
+    }
+
+}
+
