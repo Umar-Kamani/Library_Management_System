ac0bb241c8eb9c440190913743d4e1c7e0d2f39b
Sherif <sumailasherif773@gmail.com>
2026-09-10 20:48:03 +0400

    constructors , getters and models for location updated

--- src/main/java/Models/Location.java
+++ src/main/java/Models/Location.java
@@ -2,12 +2,25 @@ package Models;
 
 public class Location {
 
-public void main {
+        //this is the  unique book id
+        private int id;
+
+        // the actual location name, e.g. "Shelf A2" or "Floor 1 - Reference Section"
+        private String name;
+
+        // constructor, fills both fields when a Location object is created
+        public Location(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
 
 
+        public int getId() {return id;}
+        public void setId(int id) {this.id = id;}
 
-
+        public String getName() {return name;}
+        public void setName(String name) {this.name = name;}
     }
 
 
-}
+
