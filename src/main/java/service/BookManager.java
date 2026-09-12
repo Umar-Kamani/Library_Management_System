468ae3a097e630a4244d53634f493094bf7fed20
Sherif <sumailasherif773@gmail.com>
2026-09-12 15:36:27 +0400

    SQl statement to insert

--- src/main/java/service/BookManager.java
+++ src/main/java/service/BookManager.java
@@ -8,7 +8,14 @@ import java.sql.SQLException;
 
 public class BookManager {
 
+
+    // This adds a new book to our database; it returns true if it worked, false if something went wrong
 public boolean addBook(String title, String author, String isbn,
                            int genreId, int locationId, int totalCopies) {
+
+    // to insert a new row into the books table, if there is no id,the database auto-generates that on insert availablecopies is  equal to totalcopies since nothing is borrowed yet
+    String sql = "INSERT INTO books (title, author, isbn, genreid, locationid, totalcopies, availablecopies) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?)";
+                           }
 
 }
