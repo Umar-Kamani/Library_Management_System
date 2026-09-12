a1f95753d9f89faa572a734573fe9d1ee0db498a
Sherif <sumailasherif773@gmail.com>
2026-09-12 15:32:41 +0400

    addbook variable

--- src/main/java/service/BookManager.java
+++ src/main/java/service/BookManager.java
@@ -1,6 +1,14 @@
+package service;
+
+import Models.book;
+import database.dbUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class BookManager {
 
-
-
+public boolean addBook(String title, String author, String isbn,
+                           int genreId, int locationId, int totalCopies) {
 
 }
