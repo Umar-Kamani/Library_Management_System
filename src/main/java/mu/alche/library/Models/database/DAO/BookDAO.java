dc039d3636c4e4227539dc2dcec0a83760268f06
Umar Kamani <umar.kamani@gmail.com>
2026-09-13 17:17:50 +0400

    Implementation of BookDAO extending DAO Class

---
+++ src/main/java/mu/alche/library/Models/database/DAO/BookDAO.java
@@ -0,0 +1,16 @@
+package mu.alche.library.Models.database.DAO;
+import mu.alche.library.Models.Book;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
+public interface BookDAO extends DAO<Book> {
+
+    List<Book> findByAuthor(String author) throws SQLException;
+    List<Book> findByTitle(String title)  throws SQLException;
+    List<Book> findByGenre(String genre)  throws SQLException;
+    List<Book> findByLocation(int locationId)   throws SQLException;
+    List<Book> findByIsbn(String isbn) throws SQLException;
+
+}
