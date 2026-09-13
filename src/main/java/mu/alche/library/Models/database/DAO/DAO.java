b4477ccec577d547f4377c68816559a82513bcd3
Umar Kamani <umar.kamani@gmail.com>
2026-09-13 17:14:25 +0400

    Data Access Object (DAO) interface

---
+++ src/main/java/mu/alche/library/Models/database/DAO/DAO.java
@@ -0,0 +1,11 @@
+package mu.alche.library.Models.database.DAO;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DAO<T> {
+    T create(T t) throws SQLException;
+    T update(T t) throws SQLException;
+    void delete(T t) throws SQLException;
+    T get(int id)  throws SQLException;
+    List<T> getAll()  throws SQLException;
+}
