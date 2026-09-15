cb6601c914f0604f174fd2fd683f16a9ac4ebd70
Umar Kamani <umar.kamani@gmail.com>
2026-09-15 17:11:31 +0400

    Implemented User DAO Modules

---
+++ src/main/java/mu/alche/library/Database/DAO/UserDAO.java
@@ -0,0 +1,9 @@
+package mu.alche.library.Database.DAO;
+
+import mu.alche.library.Models.Student;
+import mu.alche.library.Models.User;
+
+import java.sql.SQLException;
+
+public interface UserDAO extends DAO<User> {
+}
