d8d11a0a65590fdc30bb2df8473ec9800d17ec5d
Umar Kamani <umar.kamani@gmail.com>
2026-09-17 20:43:13 +0400

    Added custom button UI component

---
+++ src/main/java/mu/alche/library/App/UIComponents.java
@@ -0,0 +1,20 @@
+package mu.alche.library.App;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UIComponents {
+
+    static JButton createButton(String btn_title, String btn_color) {
+
+        JButton btn = new JButton(btn_title);
+        btn.setFocusPainted(false);
+        btn.setBackground(Color.decode(btn_color));
+        btn.setBorderPainted(false);
+        btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btn.setFont(new Font("Arial", Font.BOLD, 20));
+        btn.setForeground(Color.white);
+        return btn;
+    }
+
+}
