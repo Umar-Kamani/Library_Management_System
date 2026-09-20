253fc79b1e4fa17f3f00789e1b682e91f3810931
Sherif <sumailasherif773@gmail.com>
2026-09-20 12:49:13 +0400

    Genre service added

--- src/main/java/mu/alche/library/Service/GenreService.java
+++ src/main/java/mu/alche/library/Service/GenreService.java
@@ -1,4 +1,38 @@
 package mu.alche.library.Service;
 
+import mu.alche.library.Database.DAO.GenreDAO;
+import mu.alche.library.Models.Genre;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
+
 public class GenreService {
-}
+    private static final int MAX_NAME_LENGTH = 255;
+
+    private final GenreDAO genreDAO;
+
+    public GenreService(GenreDAO genreDAO) {
+        this.genreDAO = genreDAO;
+    }
+
+    public Genre createGenre(Genre genre) throws SQLException {
+
+        if (genre == null) {
+            throw new IllegalArgumentException("Genre cannot be empty");
+        }
+
+        String name = validateName(genre.getName());
+
+        if (genreDAO.findByName(name) != null) {
+            throw new IllegalArgumentException(
+                    "A genre named \"" + name + "\" already exists"
+            );
+
+        }
+
+        genre.setName(name);
+
+        return genreDAO.create(genre);
+    }
+
