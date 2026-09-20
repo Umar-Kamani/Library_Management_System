bde84a5a42dff743344a43b18bd281e18c57705f
Nellyvine <n.mizero@alustudent.com>
2026-09-20 15:30:11 +0400

    Made some edits in DBUtils.java, Launcher.java, and pom.xml

--- src/main/java/mu/alche/library/App/Launcher.java
+++ src/main/java/mu/alche/library/App/Launcher.java
@@ -1,5 +1,6 @@
 package mu.alche.library.App;
 
+import mu.alche.library.Database.DBUtils;
 
 import javax.swing.*;
 import java.sql.SQLException;
@@ -7,6 +8,8 @@ import java.sql.SQLException;
 public class Launcher {
 
     public static void main(String args[]) throws SQLException, ClassNotFoundException {
+        new DBUtils().initialiseSchema();
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
