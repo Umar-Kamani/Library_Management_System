7ede0cb47de235154338650ff6e91bfcd9011dd0
Umar Kamani <umar.kamani@gmail.com>
2026-09-20 09:49:17 +0400

    Implemented LocationDAO

---
+++ src/main/java/mu/alche/library/Database/DAO/LocationDAO.java
@@ -0,0 +1,9 @@
+package mu.alche.library.Database.DAO;
+import mu.alche.library.Models.Location;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface LocationDAO extends DAO<Location> {
+    List<Location> findChildren() throws SQLException;
+}
